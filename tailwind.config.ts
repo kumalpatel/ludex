cd273b67ae5da4f596983c9b53cc89cb8a522de2
kumalpatel <kumal.patel21@gmail.com>
2025-01-01 19:02:51 -0500

    chore: add assets and configure fonts and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,35 @@
 import type { Config } from 'tailwindcss'
 
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
 const config: Config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        general: ['general', ...fontFamily.sans],
+        zentry: ['zentry', ...fontFamily.sans],
+        'circular-web': ['circular-web', ...fontFamily.sans],
+        'robert-medium': ['robert-medium', ...fontFamily.sans],
+        'robert-regular': ['robert-regular', ...fontFamily.sans],
+      },
+      colors: {
+        blue: {
+          50: '#DFDFF0',
+          75: '#DFDFF2',
+          100: '#F0F2FA',
+          200: '#010101',
+          300: '#4FB7DD',
+        },
+        violet: {
+          300: '#5724FF',
+        },
+        yellow: {
+          100: '#8E983F',
+          300: '#EDFF66',
+        },
+      },
+    },
   },
   plugins: [],
 }
